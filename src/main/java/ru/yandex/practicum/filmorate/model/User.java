7c2563b2e86436fbd23952189cc700c90cef69f6
chugunse <chugunse@yandex.ru>
2022-12-13 13:37:52 +0300

    first

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class User {
+    private int id;
+    @Email
+    private final String email;
+    @NotBlank
+    @NonNull
+    private final String login;
+    private String name;
+    private final String birthday;
+}
